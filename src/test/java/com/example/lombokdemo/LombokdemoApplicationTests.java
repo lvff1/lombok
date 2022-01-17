c8a68b99737474f53480acae78500e4a40982aa8
齐天大圣 <1124421329@qq.com>
2022-01-17 09:48:02 +0800

    init lombok demo

---
+++ src/test/java/com/example/lombokdemo/LombokdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lombokdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
