286f7533cbd8d3374fc09fc0629159fd76a1f5d4
齐天大圣 <1124421329@qq.com>
2022-01-17 09:51:22 +0800

    init lombok demo

--- src/main/java/com/example/lombokdemo/LombokDemoApplication.java
+++ src/main/java/com/example/lombokdemo/LombokDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LombokdemoApplication {
+public class LombokDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LombokdemoApplication.class, args);
+        SpringApplication.run(LombokDemoApplication.class, args);
     }
 
 }
